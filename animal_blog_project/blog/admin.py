50cce9a3ef28c5d373490dbb8b984ccb930770d1
dsowles <d_sowles@mail.fhsu.edu>
2025-11-09 18:32:53 -0600

    Registered animal model with admin site.

--- animal_blog_project/blog/admin.py
+++ animal_blog_project/blog/admin.py
@@ -5,4 +5,18 @@
 
 from django.contrib import admin
 
+# Blog app model imports
+from .models import Animal
+
 # Register your models here.
+
+# This decorator tells Django to use the following class def to register the animal model
+# with the Django admin site.
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    # Column names for admin table.
+    list_display = ('name', 'species', 'date_added')
+    # The following will be used by Django to setup a search bar.
+    search_fields = ('name', 'species')
+    # Adds a sidebar fileter.
+    list_filter = ('species',)
