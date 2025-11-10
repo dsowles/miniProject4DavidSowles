612e98d062e98ee21b919d4f0895f6ea3cc72b3d
dsowles <d_sowles@mail.fhsu.edu>
2025-11-09 18:03:56 -0600

    Added animal model to blog app.

--- animal_blog_project/blog/models.py
+++ animal_blog_project/blog/models.py
@@ -6,3 +6,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Animal class for use in blog app.
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField()
+    image_url = models.URLField(blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
