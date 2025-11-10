2a84a1d915a080b20152fa63a0fedf12d1530d3b
dsowles <d_sowles@mail.fhsu.edu>
2025-11-09 18:41:26 -0600

    Added Animal List and Animal Details View Functions

--- animal_blog_project/blog/views.py
+++ animal_blog_project/blog/views.py
@@ -3,6 +3,14 @@
 # Project: Mini Project 4
 # Date of Creation 11/07/2025
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Animal
 
 # Create your views here.
+def animal_list(request):
+    animals = Animal.objects.all()
+    return render(request, "blog/animal_list.html", {"animals": animals})
+
+def animal_detail(request, pk):
+    animal = get_object_or_404(Animal, pk=pk)
+    return render(request, "blog/animal_detail.html", {"animal": animal})
